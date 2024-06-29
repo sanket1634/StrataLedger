e254fa4c456b5be8c1dc7c253ef8192fce7c11cc
sanket radkar <kulkarnirutvikvishwas@gmail.com>
2024-06-30 01:38:08 +0530

    Basic changes - Home screen added

---
+++ app/src/main/java/com/example/strataledger/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.strataledger;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show());
+    }
+}
